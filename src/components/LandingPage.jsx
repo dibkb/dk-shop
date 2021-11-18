0825ade850b3091d6cb3b36f06dc16a25c932dc3
dibkb <dibas9110@gmail.con>
2021-11-18 21:42:01 +0530

    asthetic changes to landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./LandingPage.scss";
 const LandingPage = () => {
   const imageLink =
-    "https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80";
+    "https://images7.alphacoders.com/118/thumb-1920-1188302.jpg";
   const bgImage = {
     width: "100%",
     minHeight: "100vh",
@@ -14,7 +14,7 @@ const LandingPage = () => {
   };
   return (
     <div className="landing-container" style={bgImage}>
-      <h1>DK TECH STORE</h1>
+      <h1>DK   TECH    STORE</h1>
       <Link to = '/items'>
          <button>Visit Store</button></Link>
     </div>
