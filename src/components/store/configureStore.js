abf21c326a08b0bb4e4ebb6a9665b8d6ca0d6035
dibkb <dibas9110@gmail.con>
2021-11-18 18:49:04 +0530

    fixing issue

--- src/components/store/configureStore.js
+++ src/components/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import cartReducer from "./reducers/cartReducer";
 
 export function configureStore(){
--- src/components/store/dist/configureStore.dev.js
+++ src/components/store/dist/configureStore.dev.js
@@ -7,12 +7,11 @@ exports.configureStore = configureStore;
 
 var _redux = require("redux");
 
-var _reduxDevtoolsExtension = require("redux-devtools-extension");
-
 var _cartReducer = _interopRequireDefault(require("./reducers/cartReducer"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+// import { composeWithDevTools } from "redux-devtools-extension";
 function configureStore() {
   return (0, _redux.createStore)(_cartReducer["default"]);
 }
